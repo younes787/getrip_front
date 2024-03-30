ba184189a9b0808afe9bcffc2cfb5d06ffdb0c6f
dania <alsmandania826@gmail.com>
2024-03-30 06:48:36 +0300

    add dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,45 @@
+import { Card } from "primereact/card";
+import DashboardChart from "../components/Chart";
+import { Button } from "primereact/button";
+
+
+const Dashboard = () =>{
+
+return(
+    <>
+    <span>
+        <h1 className="text-center text-4xl get-rp">Welcome In  Ge<span className="secondery">t</span>rip</h1>  
+          </span>
+    <DashboardChart/>
+    <Card>
+    <div className="grid gap-7 ml-3">
+    <Button
+        rounded
+        label="Manage Users"
+        icon="pi pi-user"
+        // onClick={() => setshow(true)}
+        className="pr_btn"
+      />
+      <Button
+        rounded
+        label="Manage Services"
+        icon="pi pi-user"
+        // onClick={() => setshow(true)}
+        className="pr_btn"
+      />
+      <Button
+        rounded
+        label="Manage Payment"
+        icon="pi pi-user"
+        // onClick={() => setshow(true)}
+        className="pr_btn"
+      />
+    </div>
+    
+    </Card>
+    </>
+)
+
+}
+
+export default Dashboard;
--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -2,16 +2,35 @@ import "primereact/resources/themes/lara-light-purple/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import NavBar from "../components/Navbar";
+import { Bounce, ToastContainer } from "react-toastify";
+import Dashboard from "../pages/Dashboard";
 
 const Layout = () =>{
  return(
     <div>
     <NavBar/>
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+      <ToastContainer />
     <Routes>
     <Route path="/" element={<Home />}></Route>
+    <Route path="/dashboard" element={<Dashboard />}></Route>
+
     </Routes>
     </div>
  )
